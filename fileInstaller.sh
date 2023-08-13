edf928cb946a08081602d08afe7e09a2503a020b
juaCedric <jualong8@gmail.com>
2023-08-13 09:09:35 +0000

    File Installer

---
+++ fileInstaller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome to file Installer"
+echo "Please enter your project Name"
+read projectName
+mkdir $projectName  #root folder
+cd $projectName
+mkdir file1
+mkdir file2
+mkdir file3
+
