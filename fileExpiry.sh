8914054b22dfd22e331750a730941dec27d3d10d
juaCedric <jualong8@gmail.com>
2023-08-13 08:59:21 +0000

    Delete files 5days above

---
+++ fileExpiry.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#set the number of days
+days=5
+
+#cal the currents date in seconds
+current=$(date +%s)
+
+#loop through all files in th current dir
+for file in *
+do
+#check ifthe file is older than X days
+if [ -f "$file" ] && [ $(($current - $(date +%s -r "$file"))) -gt $(($days * 24 * 60 * 60)) ]
+then
+#remove the file
+rm "$file"
+fi
+done
